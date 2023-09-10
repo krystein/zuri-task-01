1fad03c6b55c16af440504e0253d37b3d2f3d924
krystein <nworahchimaobic@gmail.com>
2023-09-10 11:45:07 +0100

    task

--- script.js
+++ script.js
@@ -1,36 +1,42 @@
-const slackUserNameElement = document.querySelector(
-  '[data-testid="slackUserName"]'
-);
-const currentDayOfTheWeekElement = document.querySelector(
-  '[data-testid="currentDayOfTheWeek"]'
-);
-const currentUTCTimeElement = document.querySelector(
-  '[data-testid="currentUTCTime"]'
-);
+// Declare variables for the day of the week and UTC time
+const daysOfWeek = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+const dayOfWeekElement = document.getElementById("dayOfWeek");
+const utcTimeElement = document.getElementById("utcTime");
 
-function getCurrentDayOfWeek() {
-  const daysOfWeek = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  const currentDate = new Date();
-  const dayIndex = currentDate.getDay();
-  currentDayOfTheWeekElement.textContent = `Current Day of the Week: ${daysOfWeek[dayIndex]}`;
+// Function to format a timestamp as a human-readable string in AM/PM format
+function formatTime(timestamp) {
+  const date = new Date(timestamp);
+  const hours = date.getUTCHours();
+  const minutes = date.getUTCMinutes().toString().padStart(2, "0");
+  const amPm = hours >= 12 ? "PM" : "AM";
+  const formattedHours = hours % 12 || 12; // Convert to 12-hour format
+  const formattedSeconds = date.getUTCSeconds().toString().padStart(2, "0");
+
+  return `${formattedHours}:${minutes}:${formattedSeconds} ${amPm} GMT +1`;
 }
 
-function updateCurrentUTCTime() {
+// Function to update the current day of the week and UTC time
+function updateDateTime() {
   const now = new Date();
-  const formattedTime = now.toISOString();
-  currentUTCTimeElement.textContent = `${formattedTime}`;
+  const dayOfWeek = daysOfWeek[now.getUTCDay()];
+
+  // Adjust the timestamp for GMT +1 (1 hour ahead of UTC)
+  now.setHours(now.getHours() + 1);
+
+  const utcTime = formatTime(now.getTime());
+
+  dayOfWeekElement.textContent = ` ${dayOfWeek}`;
+  utcTimeElement.textContent = ` ${utcTime}`;
 }
 
-updateCurrentUTCTime();
-setInterval(updateCurrentUTCTime, 1000);
-
-getCurrentDayOfWeek();
-setInterval(getCurrentDayOfWeek, 1000);
+// Update the date and time immediately and then every second
+updateDateTime();
+setInterval(updateDateTime, 1000);
